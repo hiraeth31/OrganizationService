a111fd83c156ea67d8759dae60739da27d0c44d2
hiraeth31 <beautiful_distress@outlook.com>
2025-08-22 22:09:15 +1000

    feat: add department location and department position entities

---
+++ src/OrganizationService.Domain/DepartmentPositionManagement/DepartmentPosition.cs
@@ -0,0 +1,9 @@
+﻿namespace OrganizationService.Domain.DepartmentPositionManagement
+{
+    public sealed class DepartmentPosition
+    {
+        public Guid Id { get; set; }
+        public Guid DepartmentId { get; set; }
+        public Guid PositionId { get; set; }
+    }
+}
