1a2ff808e1c63829c78345fdb7e2e77c7b6c381a
hiraeth31 <beautiful_distress@outlook.com>
2025-08-22 22:08:28 +1000

    feat: add position entity and value objects

---
+++ src/OrganizationService.Domain/PositionManagement/ValueObjects/Description.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace OrganizationService.Domain.PositionManagement.ValueObjects
+{
+    public record Description
+    {
+        private const int MAX_LENGTH = 1000;
+        private Description(string value)
+        {
+            Value = value;
+        }
+        public string Value { get; }
+
+        public static Result<Description> Create(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return Result.Failure<Description>("Не может быть пустым.");
+
+            if (value.Length >= MAX_LENGTH)
+                return Result.Failure<Description>("Не может быть более 1000 символов.");
+
+            return Result.Success(new Description(value));
+        }
+    }
+}
