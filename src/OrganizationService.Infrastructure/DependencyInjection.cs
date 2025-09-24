846b92d6f4375b315e20498f0094de224dab9ae6
hiraeth31 <beautiful_distress@outlook.com>
2025-09-24 18:23:22 +1000

    feat: add DI and application db context

---
+++ src/OrganizationService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OrganizationService.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
