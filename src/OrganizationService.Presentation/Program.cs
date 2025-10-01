5f6ade7428f15298e485a5add5691df3aeebf0c4
hiraeth31 <beautiful_distress@outlook.com>
2025-10-01 21:20:15 +1000

    feat: add serilog & seq

--- src/OrganizationService.Presentation/Program.cs
+++ src/OrganizationService.Presentation/Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
 using OrganizationService.Application;
 using OrganizationService.Infrastructure;
+using OrganizationService.Presentation.Middlewares;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") ?? throw new ArgumentNullException("Seq"))
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSerilog();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 builder.Services
     .AddInfrastructure()
@@ -13,12 +33,16 @@ builder.Services
 
 var app = builder.Build();
 
+app.UseExceptionMiddleware();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
